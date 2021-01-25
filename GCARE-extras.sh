fb3fc28f3e4b424076ae938ed52fb600637fecf4
Joe <Joe>
2021-01-25 17:22:17 +0000

    Added option to create shortcut to run script from desktop, great for touchscreens!
    GCARE-extras.sh allows for a shortcut to be placed on the desktop that can then be executed by clicking on it. Run script had to be modified to allow this to work.

---
+++ GCARE-extras.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# These extras are designed specifically to setup the use cases for our group, though others may find them useful.
+RUN_SH=$(readlink -f "./run.sh")
+echo "Creating desktop shortcut"
+echo "#!/usr/bin/env xdg-open" > ~/Desktop/OPCN3.desktop
+echo "" >> ~/Desktop/OPCN3.desktop
+echo "[Desktop Entry]" >> ~/Desktop/OPCN3.desktop
+echo "Version=0" >> ~/Desktop/OPCN3.desktop
+echo "Type=Application" >> ~/Desktop/OPCN3.desktop
+echo "Terminal=true" >> ~/Desktop/OPCN3.desktop
+echo "Exec=$RUN_SH" >> ~/Desktop/OPCN3.desktop
+echo "Name=OPCN3" >> ~/Desktop/OPCN3.desktop
+echo "Comment=" >> ~/Desktop/OPCN3.desktop
+echo "Icon=" >> ~/Desktop/OPCN3.desktop
+
+sudo chmod +x ~/Desktop/OPCN3.desktop  # Make it executable
+gio set ~/Desktop/OPCN3.desktop metadata::trusted true  # Allows user to click on shortcut to run it
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+cd $(dirname $0)
+
 PYTHON_VENV=OPCN3/bin/python3
 
 $PYTHON_VENV main.py
+
+read -p "Press Enter to close" reply
